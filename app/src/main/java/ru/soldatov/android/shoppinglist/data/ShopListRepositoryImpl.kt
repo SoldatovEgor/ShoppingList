851e47aac02d3d73be7080ce902d969badf419cb
Егор Солдатов <soldatovea93@gmail.com>
2021-12-27 21:49:27 +0300

    creating data layer

---
+++ app/src/main/java/ru/soldatov/android/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package ru.soldatov.android.shoppinglist.data
+
+import ru.soldatov.android.shoppinglist.domain.ShopItem
+import ru.soldatov.android.shoppinglist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID)
+            shopItem.id = autoIncrementId++
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with $shopItemId not find")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+
+}
