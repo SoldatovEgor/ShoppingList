d4e4c8e3e7631b579041b2363880e52742ec2d71
Егор Солдатов <soldatovea93@gmail.com>
2021-12-27 23:00:23 +0300

    creating data layer

---
+++ app/src/main/java/ru/soldatov/android/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,30 @@
+package ru.soldatov.android.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.soldatov.android.shoppinglist.data.ShopListRepositoryImpl
+import ru.soldatov.android.shoppinglist.domain.DeleteShopItemUseCase
+import ru.soldatov.android.shoppinglist.domain.EditShopItemUseCase
+import ru.soldatov.android.shoppinglist.domain.GetShopListUseCase
+import ru.soldatov.android.shoppinglist.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val shopItemCopy = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(shopItemCopy)
+    }
+}
